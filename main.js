3c88fdaef890b3784e0859566e3c5bc193856586
yvanddniyo <nyvan80076@gmail.com>
2024-03-08 21:22:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const express =  require('express');
+const mongoose = require('mongoose')
+const routes = require("./routes/blogerRoute")
+ require('dotenv').config();
+
+
+
+
+// connect to the mongoDB
+
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+
+/*new routes*/
+const app = express();
+app.use(express.json())
+app.use('/api', routes);
+
+app.listen(5000, () => {
+    console.log("Server has started");
+})
+})
