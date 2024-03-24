16b59d541746d8fd90b0daa8059a1a1eab139640
yvanddniyo <nyvan80076@gmail.com>
2024-03-24 18:32:46 +0200

    adding some changes

--- dist/routes/likeRouter.js
+++ dist/routes/likeRouter.js
@@ -1,8 +1,13 @@
 "use strict";
-// import express from "express";
-// import likeController from "../controller/likeController";
-// import tokenAuth from "../middlewares/tokenAuth";
-// const routerLikes = express.Router();
-// routerLikes.post("/blogs/:id/likes", tokenAuth, likeController.likeBlog);
-// routerLikes.get("/blogs/:id/likes", likeController.viewLikes);
-// export default routerLikes;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const userAccess_1 = __importDefault(require("../middlewares/userAccess"));
+const likeController_1 = require("../controller/likeController");
+const routerLikes = express_1.default.Router();
+router.post("/blogs/:id/likes", userAccess_1.default, likeController_1.like);
+router.get("/blogs/:id/likes", userAccess_1.default, likeController_1.getLikes);
+exports.default = routerLikes;
